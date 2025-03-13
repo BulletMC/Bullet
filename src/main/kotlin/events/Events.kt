716bc8ede653a8e57df62825e49a9c3ce2d11346
aznos <pigiotyreal@gmail.com>
2025-03-13 19:25:22 -0400

    Added a block break/place event, and a sprint/sneak event

--- src/main/kotlin/events/Events.kt
+++ src/main/kotlin/events/Events.kt
@@ -1,6 +1,8 @@
 package com.aznos.events
 
 import com.aznos.GameState
+import com.aznos.entity.player.Player
+import com.aznos.entity.player.data.Position
 
 //Unmodifiable events
 
@@ -47,6 +49,22 @@ data class PlayerHeartbeatEvent(val username: String) : Event()
  */
 data class PlayerChatEvent(val username: String, val message: String) : Event()
 
+/**
+ * Called when a player sprints
+ *
+ * @param username The username of the player that is sprinting
+ * @param isSprinting True if they're now sprinting, false if they stopped
+ */
+data class PlayerSprintEvent(val username: String, val isSprinting: Boolean) : Event()
+
+/**
+ * Called when a player sneaks
+ *
+ * @param username The username of the player that is sneaking
+ * @param isSneaking True if they're now sneaking, false if they stopped
+ */
+data class PlayerSneakEvent(val username: String, val isSneaking: Boolean) : Event()
+
 //Modifiable events
 
 /**
@@ -57,3 +75,41 @@ data class PlayerChatEvent(val username: String, val message: String) : Event()
  * @param motd The description of the server
  */
 data class StatusRequestEvent(var maxPlayers: Int, var onlinePlayers: Int, var motd: String) : Event()
+
+/**
+ * Called when the server receives a dig packet from the client
+ *
+ * @param player The player that is breaking the block
+ * @param status (0-9) The progress of the block break, if it is anything but 0-9 the block can be considered broken
+ * @param location The position of the block
+ * @param face The face of the block being hit
+ */
+data class BlockBreakEvent(
+    val player: Player,
+    var status: Int,
+    var location: Position,
+    val face: Int
+) : Event()
+
+/**
+ * Called when the server receives a block place packet from the client
+ *
+ * @param player The player that is placing the block
+ * @param hand Which hand the block was placed with
+ * @param location The position of the block being placed
+ * @param face The face of the block being placed
+ * @param cursorPosX The X position of the crosshair on the block, 0-1
+ * @param cursorPosY The Y position of the crosshair on the block, 0-1
+ * @param cursorPosZ The Z position of the crosshair on the block, 0-1
+ * @param insideBlock Whether the players head is inside a block
+ */
+data class BlockPlaceEvent(
+    val player: Player,
+    var hand: Int,
+    var location: Position,
+    val face: Int,
+    val cursorPosX: Float,
+    val cursorPosY: Float,
+    val cursorPosZ: Float,
+    val insideBlock: Boolean
+) : Event()
--- src/main/kotlin/packets/PacketHandler.kt
+++ src/main/kotlin/packets/PacketHandler.kt
@@ -66,20 +66,36 @@ class PacketHandler(
     fun onPlayerAction(packet: ClientEntityActionPacket) {
         when(packet.actionID) {
             0 -> { //Start sneaking
+                val event = PlayerSneakEvent(client.player.username, true)
+                EventManager.fire(event)
+                if(event.isCancelled) return
+
                 client.player.isSneaking = true
                 updateEntityMetadata(client.player, 6, 5)
             }
 
             1 -> { //Stop sneaking
+                val event = PlayerSneakEvent(client.player.username, false)
+                EventManager.fire(event)
+                if(event.isCancelled) return
+
                 client.player.isSneaking = false
                 updateEntityMetadata(client.player, 6, 0)
             }
 
             3 -> { //Start sprinting
+                val event = PlayerSprintEvent(client.player.username, true)
+                EventManager.fire(event)
+                if(event.isCancelled) return
+
                 sprinting.add(client.player.entityID)
             }
 
             4 -> { //Stop sprinting
+                val event = PlayerSprintEvent(client.player.username, false)
+                EventManager.fire(event)
+                if(event.isCancelled) return
+
                 sprinting.remove(client.player.entityID)
             }
         }
@@ -90,28 +106,33 @@ class PacketHandler(
      */
     @PacketReceiver
     fun onPlayerDig(packet: ClientDiggingPacket) {
-        if(client.player.gameMode == GameMode.CREATIVE && packet.status == BlockStatus.STARTED_DIGGING.id) {
+        val event = BlockBreakEvent(
+            client.player,
+            packet.status,
+            Position(packet.location.x, packet.location.y, packet.location.z),
+            packet.face
+        )
+        EventManager.fire(event)
+        if(event.isCancelled) return
+
+        if(client.player.gameMode == GameMode.CREATIVE && event.status == BlockStatus.STARTED_DIGGING.id) {
             for(otherPlayer in Bullet.players) {
                 if(otherPlayer != client.player) {
                     otherPlayer.sendPacket(ServerBlockChangePacket(
-                        Position(
-                            packet.location.x,
-                            packet.location.y,
-                            packet.location.z
-                        ),
+                        event.location,
                         0
                     ))
                 }
             }
         } else if(client.player.gameMode == GameMode.SURVIVAL) {
-            when(packet.status) {
+            when(event.status) {
                 BlockStatus.STARTED_DIGGING.id -> {
                     val breakTime = getStoneBreakTime()
-                    startBlockBreak(packet.location, breakTime.toInt())
+                    startBlockBreak(event.location, breakTime.toInt())
                 }
 
                 BlockStatus.CANCELLED_DIGGING.id, BlockStatus.FINISHED_DIGGING.id -> {
-                    stopBlockBreak(packet.location)
+                    stopBlockBreak(event.location)
                 }
             }
         }
@@ -131,24 +152,35 @@ class PacketHandler(
      */
     @PacketReceiver
     fun onBlockPlacement(packet: ClientBlockPlacementPacket) {
-        val blockLocation = packet.location
+        val event = BlockPlaceEvent(
+            client.player,
+            packet.hand,
+            Position(packet.location.x, packet.location.y, packet.location.z),
+            packet.face,
+            packet.cursorPositionX,
+            packet.cursorPositionY,
+            packet.cursorPositionZ,
+            packet.insideBlock
+        )
+        EventManager.fire(event)
+        if(event.isCancelled) return
 
-        when(packet.face) {
-            0 -> blockLocation.y -= 1
-            1 -> blockLocation.y += 1
-            2 -> blockLocation.z -= 1
-            3 -> blockLocation.z += 1
-            4 -> blockLocation.x -= 1
-            5 -> blockLocation.x += 1
+        when(event.face) {
+            0 -> event.location.y -= 1
+            1 -> event.location.y += 1
+            2 -> event.location.z -= 1
+            3 -> event.location.z += 1
+            4 -> event.location.x -= 1
+            5 -> event.location.x += 1
         }
 
         for(otherPlayer in Bullet.players) {
             if(otherPlayer != client.player) {
                 otherPlayer.sendPacket(ServerBlockChangePacket(
                     Position(
-                        blockLocation.x,
-                        blockLocation.y,
-                        blockLocation.z
+                        event.location.x,
+                        event.location.y,
+                        event.location.z
                     ),
                     2 //TODO: Check what block the player is holding, and update this
                 ))
