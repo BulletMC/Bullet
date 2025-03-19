9b4e0e603534667be779fa7caa9a8b2e8353af5e
aznos <pigiotyreal@gmail.com>
2025-03-19 12:48:45 -0400

    Server boss bar packet

---
+++ src/main/kotlin/packets/play/out/ServerBossBarPacket.kt
@@ -0,0 +1,76 @@
+package com.aznos.packets.play.out
+
+import com.aznos.datatypes.StringType.writeString
+import com.aznos.datatypes.UUIDType.writeUUID
+import com.aznos.datatypes.VarInt.writeVarInt
+import com.aznos.packets.Packet
+import com.aznos.packets.data.BossBarColor
+import com.aznos.packets.data.BossBarDividers
+import java.util.UUID
+
+/**
+ * This packet sends a boss bar to the client
+ *
+ * @param uuid The UUID of the boss bar
+ * @param action The action to perform on the boss bar
+ * @param title The title of the boss bar
+ * @param health The health of the boss bar, 0-1
+ * @param color The color of the boss bar
+ * @param division The amount of notches in the boss bar
+ * @param flags Optional flags, 0x1 - Darken sky, 0x2 - Dragon bar (used to play end music), 0x04 - Create fog
+ */
+class ServerBossBarPacket(
+    uuid: UUID,
+    action: Int,
+    title: String? = null,
+    health: Float? = null,
+    color: BossBarColor? = null,
+    division: BossBarDividers? = null,
+    flags: Byte? = null
+) : Packet(0x0C) {
+    init {
+        wrapper.writeUUID(uuid)
+        wrapper.writeVarInt(action)
+
+        when(action) {
+            0 -> { //Add
+                requireNotNull(title)
+                requireNotNull(health)
+                requireNotNull(color)
+                requireNotNull(division)
+
+                wrapper.writeString(title)
+                wrapper.writeFloat(health)
+                wrapper.writeVarInt(color.id)
+                wrapper.writeVarInt(division.id)
+
+                if(flags != null) {
+                    wrapper.writeByte(flags.toInt())
+                }
+            }
+            1 -> { //Remove
+
+            }
+            2 -> { //Update health
+                requireNotNull(health)
+                wrapper.writeFloat(health)
+            }
+            3 -> { //Update title
+                requireNotNull(title)
+                wrapper.writeString(title)
+            }
+            4 -> { //Update style
+                requireNotNull(color)
+                requireNotNull(division)
+
+                wrapper.writeVarInt(color.id)
+                wrapper.writeVarInt(division.id)
+            }
+            5 -> { //Update flags
+                requireNotNull(flags)
+                wrapper.writeByte(flags.toInt())
+
+            }
+        }
+    }
+}
