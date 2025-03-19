f6602d7fa4c39a128c3accac8a3d3e0e8124b973
aznos <pigiotyreal@gmail.com>
2025-03-19 13:05:33 -0400

    Made detekt happy

--- src/main/kotlin/entity/player/Player.kt
+++ src/main/kotlin/entity/player/Player.kt
@@ -132,14 +132,23 @@ class Player(
      * Used to send a boss bar to the player
      *
      * @param title The title of the boss bar
-     * @param health The health of the boss bar, 0-1. It's possible to go over 1, and around 1.5 it'll start to show a second boss bar
+     * @param health The health of the boss bar, 0-1. It's possible to go over 1
+     * and around 1.5 it'll start to show a second boss bar
      * @param color The color of the boss bar
      * @param dividers How many dividers should show on the boss bar
      * @param darkenSky If the boss bar should darken the sky
      * @param playEndMusic If the boss bar should play end music (Used for the ender dragon)
      * @param createFog If the boss bar should create fog
      */
-    fun addBossBar(title: String, health: Float? = 1f, color: BossBarColor? = BossBarColor.PINK, dividers: BossBarDividers? = BossBarDividers.NONE, darkenSky: Boolean = false, playEndMusic: Boolean = false, createFog: Boolean = false) {
+    fun addBossBar(
+        title: String,
+        health: Float? = 1f,
+        color: BossBarColor? = BossBarColor.PINK,
+        dividers: BossBarDividers? = BossBarDividers.NONE,
+        darkenSky: Boolean = false,
+        playEndMusic: Boolean = false,
+        createFog: Boolean = false
+    ) {
         val uuid = UUID.randomUUID()
         bossBars.add(uuid)
 
@@ -172,7 +181,17 @@ class Player(
      * @param playEndMusic If the boss bar should play end music (Used for the ender dragon)
      * @param createFog If the boss bar should create fog
      */
-    fun updateBossBar(id: Int, action: ServerBossBarPacket.Action, title: String? = null, health: Float? = null, color: BossBarColor? = null, dividers: BossBarDividers? = null, darkenSky: Boolean? = null, playEndMusic: Boolean? = null, createFog: Boolean? = null) {
+    fun updateBossBar(
+        id: Int,
+        action: ServerBossBarPacket.Action,
+        title: String? = null,
+        health: Float? = null,
+        color: BossBarColor? = null,
+        dividers: BossBarDividers? = null,
+        darkenSky: Boolean? = null,
+        playEndMusic: Boolean? = null,
+        createFog: Boolean? = null
+    ) {
         val uuid = bossBars[id]
 
         when(action) {
