e5e9df2fe16a661c1bc5862662171d8dc83b71ac
aznos <pigiotyreal@gmail.com>
2025-03-19 16:34:11 -0400

    Spawn entity packet

--- src/main/kotlin/packets/play/out/ServerSpawnEntityPacket.kt
+++ src/main/kotlin/packets/play/out/ServerSpawnEntityPacket.kt
@@ -9,27 +9,27 @@ import java.util.UUID
 /**
  * Used to spawn a living entity
  * If spawning a player you should instead see [ServerSpawnPlayerPacket]
- * and if spawning a non-living entity, see [ServerSpawnEntityPacket]
+ * and if spawning a living entity, you should see [ServerSpawnLivingEntityPacket]
  *
  * @param entityID The new entityID, this should be unique
  * @param entityUUID The new UUID of the entity
  * @param type The type of entity to spawn in
  * @param location The location of the entity
- * @param headPitch The head pitch of the entity
  * @param velocityX The velocity of the entity on the X axis
  * @param velocityY The velocity of the entity on the Y axis
  * @param velocityZ The velocity of the entity on the Z axis
+ * @param data Meaning dependent on the value of the type
  */
-class ServerSpawnLivingEntityPacket(
+class ServerSpawnEntityPacket(
     entityID: Int,
     entityUUID: UUID,
     type: Int,
     location: Location,
-    headPitch: Float,
     velocityX: Short,
     velocityY: Short,
-    velocityZ: Short
-) : Packet(0x02) {
+    velocityZ: Short,
+    data: Int? = null,
+) : Packet(0x00) {
     init {
         wrapper.writeVarInt(entityID)
         wrapper.writeUUID(entityUUID)
@@ -40,7 +40,10 @@ class ServerSpawnLivingEntityPacket(
         wrapper.writeDouble(location.z)
         wrapper.writeByte((location.yaw * 256.0f / 360.0f).toInt())
         wrapper.writeByte((location.pitch * 256.0f / 360.0f).toInt())
-        wrapper.writeByte((headPitch * 256.0f / 360.0f).toInt())
+
+        if(data != null) {
+            wrapper.writeInt(data)
+        }
 
         wrapper.writeShort(velocityX.toInt())
         wrapper.writeShort(velocityY.toInt())
