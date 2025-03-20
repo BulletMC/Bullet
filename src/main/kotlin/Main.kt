8245cfc747db5a60c36f6e6bc85daa4596d7e7f8
aznos <pigiotyreal@gmail.com>
2025-03-19 20:59:48 -0400

    Remove test

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -9,10 +9,5 @@ import net.kyori.adventure.text.format.NamedTextColor
  * Application entry point
  */
 fun main() {
-    EventManager.register(PlayerJoinEvent::class.java) { e ->
-        e.player.setTabListHeader(Component.text("Bullet").color(NamedTextColor.GOLD))
-        e.player.setTabListFooter(Component.text("Runs as fast as a bullet!").color(NamedTextColor.GOLD))
-    }
-
     Bullet.createServer("0.0.0.0")
 }
