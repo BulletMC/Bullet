7b9bceae5b7fc379f59691e43cb0b9ec8a2c2285
alexcrea <42614139+alexcrea@users.noreply.github.com>
2025-03-27 10:35:49 +0100

    use enum's ordinal as CommandCodes id

--- src/main/kotlin/commands/CommandCodes.kt
+++ src/main/kotlin/commands/CommandCodes.kt
@@ -3,10 +3,15 @@ package com.aznos.commands
 /**
  * Enum class that contains the command codes that can be sent to the client
  */
-enum class CommandCodes(val id: Int) {
-    UNKNOWN(0),
-    SUCCESS(1),
-    ILLEGAL_SYNTAX(2),
-    ILLEGAL_ARGUMENT(3),
-    INVALID_PERMISSIONS(4)
+enum class CommandCodes {
+    UNKNOWN,
+    SUCCESS,
+    ILLEGAL_SYNTAX,
+    ILLEGAL_ARGUMENT,
+    INVALID_PERMISSIONS,
+    ;
+
+    val id: Int
+        get() = ordinal
+
 }
