14992ad1077b301a4cdb83807f8be10fa5a358f6
Aznos <91508926+Aznos@users.noreply.github.com>
2025-06-06 15:10:07 -0400

    Reorganized some classes, and added server spawn experience orb

---
+++ src/main/kotlin/packets/play/out/packets/play/out/ServerSpawnExperienceOrb.kt
@@ -0,0 +1,25 @@
+package com.aznos.packets.play.out.packets.play.out
+
+import com.aznos.datatypes.BlockPositionType
+import com.aznos.datatypes.BlockPositionType.writeBlockPos
+import com.aznos.datatypes.VarInt.writeVarInt
+import com.aznos.packets.Packet
+
+/**
+ * Spawns one or more experience orbs
+ *
+ * @param entityID The unique ID of the experience orb entity
+ * @param blockPos Where to spawn the experience orb
+ * @param count The amount of experience this orb will reward once collected
+ */
+class ServerSpawnExperienceOrb(
+    entityID: Int,
+    blockPos: BlockPositionType.BlockPosition,
+    count: Int
+) : Packet(0x01) {
+    init {
+        wrapper.writeVarInt(entityID);
+        wrapper.writeBlockPos(blockPos);
+        wrapper.writeShort(count);
+    }
+}
