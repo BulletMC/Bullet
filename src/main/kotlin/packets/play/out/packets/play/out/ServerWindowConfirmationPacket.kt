880ad8d841efa7b2cc81a1911c55ecc5a13059e9
Aznos <91508926+Aznos@users.noreply.github.com>
2025-06-10 17:15:28 -0400

    Server window confirmation packet

---
+++ src/main/kotlin/packets/play/out/packets/play/out/ServerWindowConfirmationPacket.kt
@@ -0,0 +1,24 @@
+package com.aznos.packets.play.out.packets.play.out
+
+import com.aznos.packets.Packet
+
+/**
+ * A packet from the server indicating whether a request from the client was accepted
+ * or whether there was a conflict (due to lag). If the packet was not accepted, the client
+ * must respond with a Server-Bound window confirmation packet.
+ *
+ * @param windowID The ID of the window that the action occurred in
+ * @param actionNumber Every action that is to be accepted has a unique number
+ * @param accepted Whether the action was accepted or not
+ */
+class ServerWindowConfirmationPacket(
+    windowID: Byte,
+    actionNumber: Short,
+    accepted: Boolean
+) : Packet(0x11) {
+    init {
+        wrapper.writeByte(windowID.toInt())
+        wrapper.writeShort(actionNumber.toInt())
+        wrapper.writeBoolean(accepted)
+    }
+}
