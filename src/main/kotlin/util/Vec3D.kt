e2372b5719cc51aaf07bfe8cc98403836307fca2
Aznos <91508926+Aznos@users.noreply.github.com>
2025-06-18 16:09:40 -0400

    Vec3D.kt

---
+++ src/main/kotlin/util/Vec3D.kt
@@ -0,0 +1,15 @@
+package com.aznos.util
+
+import kotlin.math.sqrt
+
+@JvmInline value class Vec3D(val raw: Triple<Double, Double, Double>) {
+    val x get() = raw.first
+    val y get() = raw.second
+    val z get() = raw.third
+
+    operator fun plus(o: Vec3D) = Vec3D(Triple(x + o.x, y + o.y, z + o.z))
+    operator fun minus(o: Vec3D) = Vec3D(Triple(x - o.x, y - o.y, z - o.z))
+
+    fun length() = sqrt(x * x + y * y + z * z)
+    fun normalize() = if(length() == 0.0) this else Vec3D(Triple(x / length(), y / length(), z / length()))
+}
