3a8c67b46e5468e7402068df5e79143abaf41000
Aznos <91508926+Aznos@users.noreply.github.com>
2025-06-19 16:43:11 -0400

    feetBox AABB in LivingEntity.kt

--- src/main/kotlin/entity/livingentity/LivingEntity.kt
+++ src/main/kotlin/entity/livingentity/LivingEntity.kt
@@ -3,6 +3,7 @@ package com.aznos.entity.livingentity
 import com.aznos.entity.Entity
 import com.aznos.entity.ai.Navigator
 import com.aznos.entity.ai.RandomStrollGoal
+import com.aznos.util.AABB
 import com.aznos.world.World
 
 class LivingEntity : Entity() {
@@ -10,8 +11,22 @@ class LivingEntity : Entity() {
     val navigator get() = navigatorLazy
 
     private val strollLazy by lazy { RandomStrollGoal() }
+
+    /**
+     * Updates the entity's AI and navigation
+     *
+     * @param world The world in which the entity exists
+     */
     fun tickAI(world: World) {
         strollLazy.tick(world, this)
         navigator.tick(world)
     }
+
+    /**
+     * Returns the bounding box of the entity's feet
+     */
+    fun feetBox() = AABB(
+        location.x - 0.3, location.z - 0.3,
+        location.x + 0.3, location.z + 0.3,
+    )
 }
